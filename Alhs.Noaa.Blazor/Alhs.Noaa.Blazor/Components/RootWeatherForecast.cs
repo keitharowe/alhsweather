436c390b3a398bbb055d69ec263940696e0a0789
Keith Rowe <krowe@aztekweb.com>
2025-03-09 11:13:47 -0400

    initial commit

---
+++ Alhs.Noaa.Blazor/Alhs.Noaa.Blazor/Components/RootWeatherForecast.cs
@@ -0,0 +1,23 @@
+﻿namespace Alhs.Noaa.Blazor.Components
+{
+    public class RootWeatherForecast
+    {
+        public WeatherForecast Properties { get; set; }
+    }
+
+    public class WeatherForecast
+    {
+        public string Forecast { get; set; }
+        public RelativeLocation RelativeLocation { get; set; }
+    }
+
+    public class RelativeLocation
+    {
+        public LocationProperties Properties { get; set; }
+    }
+    public class LocationProperties
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Alhs.Noaa.Blazor/Alhs.Noaa.Blazor/Components/RootWeatherForecastDetail.cs
@@ -0,0 +1,20 @@
+﻿namespace Alhs.Noaa.Blazor.Components
+{
+    public class RootWeatherForecastDetail
+    {
+        public ForecastDetail Properties { get; set; }
+    }
+
+    public class ForecastDetail
+    {
+        public List<DetailRecord> Periods { get; set; }
+    }
+
+    public class DetailRecord
+    {
+        public decimal Temperature { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string ShortForecast { get; set; }
+    }
+}
